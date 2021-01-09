dbf7d49977afaa083139f4b533b88de0a3acb7aa
佐々木　莉奈 <1072sasaki@gmail.com>
2021-01-09 18:58:51 +0900

    Add chap2_6_2.rb ||の練習

---
+++ chap2_6_2.rb
@@ -0,0 +1,13 @@
+#||の練習
+puts "年齢は？"
+age=gets.to_i
+if age <=5 || age >=65
+  puts "幼児と高齢者"
+end
+
+#個人練習
+puts "一日のご飯の回数は？"
+number=gets.to_i
+if number <=2 || number >=6
+  puts "食べなすぎと食べすぎ"
+end
