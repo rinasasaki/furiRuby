3225a82170e3e360d9697318b4dc475fa514f450
佐々木　莉奈 <1072sasaki@gmail.com>
2021-01-09 18:28:18 +0900

    Add chap2_4_1.rb else節の練習

---
+++ chap2_4_1.rb
@@ -0,0 +1,18 @@
+#else節
+puts "年齢は？"
+age=gets.to_i
+if age<20
+  puts "未成年"
+else
+  puts "成人"
+end
+
+#個人練習
+puts "------"
+puts "テストの点数は？"
+point=gets.to_i
+if point>80
+  puts "よくできました"
+else
+  puts "もうちょっと頑張れ"
+end
