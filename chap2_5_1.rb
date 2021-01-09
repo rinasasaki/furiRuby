160b9a4da539cecd6d4f11e521a85c691d99ad19
佐々木　莉奈 <1072sasaki@gmail.com>
2021-01-09 18:34:38 +0900

    Add chap2_5_2.rb elsifの練習

--- chap2_5_1.rb
+++ chap2_5_1.rb
@@ -1,10 +1,12 @@
-#else節
+#elsif節
 puts "年齢は？"
 age=gets.to_i
 if age<20
   puts "未成年"
-else
+elsif age<65
   puts "成人"
+else
+  puts  "高齢者"
 end
 
 #個人練習
@@ -13,7 +15,8 @@ puts "テストの点数は？"
 point=gets.to_i
 if point>80
   puts "よくできました"
+elsif point>60
+  puts "頑張ったね"
 else
   puts "もうちょっと頑張れ"
 end
-
