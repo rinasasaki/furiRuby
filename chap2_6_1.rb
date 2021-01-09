4c4381a5ed78455864699fa02daebc9910c624b8
佐々木　莉奈 <1072sasaki@gmail.com>
2021-01-09 18:52:51 +0900

    Add chap2_6_1.rb　複数の条件式の練習

---
+++ chap2_6_1.rb
@@ -0,0 +1,14 @@
+#複数の条件式
+puts "年齢は？"
+age=gets.to_i
+if age>=6 && age<=15
+  puts "義務教育の対象"
+end
+
+#個人練習
+puts "----"
+puts "今日はなに食べよう？"
+menu=gets
+if gets= "餃子 && ラーメン"
+  puts "ラーメン屋さんに行こう"
+end
